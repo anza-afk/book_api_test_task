09f6c931a067891ac28d9a8da14e4d97399551fa
Anatoly Zapravadov <zapokami@gmail.com>
2023-11-23 14:16:44 +0300

    api without user

--- library/api/views.py
+++ library/api/views.py
@@ -16,25 +16,3 @@ class BookViewSet(ModelViewSet):
         permissions.AllowAny
     ]
 
-
-class CreateUserView(CreateAPIView):
-
-    model = CustomUser
-    permission_classes = [
-        permissions.AllowAny
-    ]
-    serializer_class = CustomUserSerializer
-
-    def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.save()
-        print(user.email)
-        send_hello_email_task.delay(
-            username=user.username,
-            email=user.email,
-        )
-        headers = self.get_success_headers(serializer.data)
-        return Response(
-            serializer.data, status=status.HTTP_201_CREATED, headers=headers
-        )
