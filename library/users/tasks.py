7f840e83130f6a323dc84e24a9cda537652c8b4b
anza-afk <99034105+anza-afk@users.noreply.github.com>
2023-11-24 12:38:49 +0300

    Update tasks.py

--- library/users/tasks.py
+++ library/users/tasks.py
@@ -9,7 +9,7 @@ def send_hello_email_task(username: str, email: str) -> None:
     """Отправка письма при создании нового пользователя."""
     send_mail(
         subject="Успешная регистрация.",
-        message=f"Перивет, {username}\nСпасибо за регистрацию!",
+        message=f"Привет, {username}\nСпасибо за регистрацию!",
         from_email=settings.EMAIL_HOST_USER,
         recipient_list=[email],
         fail_silently=False,
